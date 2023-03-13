c0575bbc070bfa23745ef53a7c8350874259affa
nakibcse <nakkibcse@gmail.com>
2023-03-13 09:19:22 +0600

    Javascript variable practice

---
+++ introduction-to-javascript/variable.js
@@ -0,0 +1,16 @@
+// vari + able = variable
+// numeric
+var price = 13;
+var age = 17;
+var temperature = 38;
+var money = 112;
+
+//String
+var person = "Nakib Khan";
+var location = "Andarkilla, Chattogram.";
+var special = 'String with single qoute';
+
+//Boolean
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
