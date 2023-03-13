77a95c197bd1514ca2225a84c12d3d53c76277c4
nakibcse <nakkibcse@gmail.com>
2023-03-13 11:39:28 +0600

    practice shorthand in javascript

---
+++ introduction-to-javascript/shorthand.js
@@ -0,0 +1,31 @@
+//Variable declaration 
+var tomatoPrice = 25;
+
+//Variable value update
+tomatoPrice = 30;
+
+// Add something to the previous value
+var price1 = 31;
+var price2 = 10;
+
+// += means adding some value to the existing value
+price1+=10;
+
+console.log(price1);
+
+//-= means deducting some values from the current value of the variable
+price2-=5;
+
+console.log(price2);
+
+// *= means multiplying the current values with another value
+price1*= 3;
+
+// /= means dividing the current vale by another number 
+price1/=3;
+
+// ++ means increase by 1
+price1 ++;
+
+//-- means decreasing the value by one
+price1 --;
