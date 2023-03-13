60f1425efbd32b17c8184ca87828daaa7b7b5825
nakibcse <nakkibcse@gmail.com>
2023-03-13 12:29:58 +0600

    concatenation practice

---
+++ introduction-to-javascript/concatination.js
@@ -0,0 +1,12 @@
+var price1 = 31;
+var price2 = 10;
+var tomatoPrice = price1 + price2;
+
+// console.log(tomatoPrice);
+
+var firstName = "Nakib";
+var lastName = "Khan";
+
+//String concatenation
+var fullName = firstName + ' ' +lastName;
+console.log(fullName);
