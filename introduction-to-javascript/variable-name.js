f061c57b7159712e9cd4a35d2067e8eca1dfa8a2
nakibcse <nakkibcse@gmail.com>
2023-03-13 09:41:02 +0600

    Javascript keywords and naming conventin practice

---
+++ introduction-to-javascript/variable-name.js
@@ -0,0 +1,25 @@
+//good variable
+var price = 29;
+
+// Variable name can't be any keywords
+
+var false = 96;  // Not valid
+var return = true; //Not valid
+
+
+// Variable name has to be in one word, no space 
+var my home address = "New California" //Not valid
+
+// Variable name can not have qoutation
+var "name" = "Nakib Khan"; //Not valid
+
+//Variable name can not starts with a number but can end with a number
+
+var 99club = 1964; //Not valid
+var club25 = 2025; // Valid
+
+// How to use long names
+var user_name = "raj bappa"; //Snack case
+var userHomeAddrees = "Chittagong";  //Camel case (recommendad)
+var UserHomeAddress = "Chittagong"; //Pascal case
+
