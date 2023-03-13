732add136d4ba61bd9c17f13c44f8dc40c206b09
nakibcse <nakkibcse@gmail.com>
2023-03-13 08:29:21 +0600

    intro to javascript

---
+++ introduction-to-javascript/math.js
@@ -0,0 +1,18 @@
+/***
+ * Basic mathematical knowledge you need to know for starting coding
+ * 1. number ==> 55, 898, -32
+ * 2. integer: 2, 5
+ * 3. float: decimal: 2.5, 13.67, 32.69
+ * 4. operation: addition, substraction, miltiplication, division, remainder
+ * 5. percent
+ * 6. average
+ * 7. comparison:  >, <, ==, !=, >=, <=
+ * 8. Oikik: 2 kilo apple and 5 kilo mango:
+ * 9. 45, 25,68, 129, 2: max, min, average, sum
+ * 10. Series:
+ * 11. prime 
+ * 12. permutation
+ * 13. Area: 
+ * :::::Always keep a mindset learn things if required:::: 
+ * 
+ ***/
