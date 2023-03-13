65f582290dfe53d59e1548cc6f95d14e3e432b42
nakibcse <nakkibcse@gmail.com>
2023-03-13 16:02:41 +0600

    practice type and type conversion in javascript

---
+++ introduction-to-javascript/conversion.js
@@ -0,0 +1,11 @@
+var price1 = 31;
+var price2 = '10';
+
+var total = price1 + price2;
+console.log(total);
+
+
+//convert string to number
+console.log(price1);
+
+var price1Number = parseInt(price1);
---
+++ introduction-to-javascript/type.js
@@ -0,0 +1,23 @@
+var price = 100;
+console.log(typeof price);
+
+var price = '100'
+console.log(typeof price)
+
+var isHappy = true;
+console.log(typeof isHappy);
+
+var romantic;
+console.log(typeof romantic);
+
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(1);
+console.log(typeof sum);
+sum = parseFloat(sum);
+console.log(sum);
+
+
+console.log(typeof sum);
