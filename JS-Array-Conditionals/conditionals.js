35f83d7b1f57a1429fae1f70d3082d3b23104bc1
nakibcse <nakkibcse@gmail.com>
2023-03-15 12:19:01 +0600

    practice conditional statements

---
+++ JS-Array-Conditionals/conditionals.js
@@ -0,0 +1,15 @@
+var iPhonePrice = 79000;
+var myBudget = 950000;
+
+if(iPhonePrice < myBudget){
+    console.log("I can buy iPhone");
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+if(chickenPrice <= myMoney){
+    console.log('I can buy a chicken!! Yahooo!');
+}
+else{
+    console.log("I can't buy a chicken!! So sad");
+}
---
+++ JS-Array-Conditionals/multiple-conditions.js
@@ -0,0 +1,32 @@
+var isGraduated = true;
+var salary = 25000;
+var car = 1;
+//Checks single condition
+if(isGraduated == true){
+    console.log("I will get married! Yahooo!!");
+}
+else
+{
+    console.log("Wait to get married!");
+}
+
+//Checks double condition
+if(isGraduated == true && salary > 50000){
+    console.log("I will get married! Yahooo!!");
+}
+else
+{
+    console.log("Wait to get married!");
+}
+
+
+//Checks multiple conditions 
+if(isGraduated == true && salary > 50000 && car >=1){
+    console.log("I'm gonna marry...!");
+}
+else{
+    console.log("I have to wait to get married");
+}
+
+
+
