2a8d49924738642b599575f3228158519f57aa8f
nakibcse <nakkibcse@gmail.com>
2023-03-15 10:26:54 +0600

    practice push pop

---
+++ JS-Array-Conditionals/push-pop.js
@@ -0,0 +1,20 @@
+var numbers = [78,45,98,45];
+
+// use push to add an element to an array as the last element of the array
+numbers.push(63);
+console.log(numbers);
+
+var friends = ['Balam', 'Kalam', 'Salam'];
+friends.push("Nakib");
+console.log(friends);
+
+
+// use pop to get last element of the array
+var element = numbers.pop();
+console.log(element);
+console.log(numbers);
+
+//use unshift to add an element at the beginning of the array
+numbers.unshift(10000);
+
+console.log(numbers);
