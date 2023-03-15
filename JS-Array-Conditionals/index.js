f6e20c2d27a41417cd331cd6fb8a073dd3884d28
nakibcse <nakkibcse@gmail.com>
2023-03-15 09:52:12 +0600

    Practice Array declaration and indeingx

---
+++ JS-Array-Conditionals/index.js
@@ -0,0 +1,14 @@
+var number = [45, 68, 78, 56, 89, 98];
+
+// Get element by index
+var element = number[1]; 
+console.log(element);
+
+//Set element value by index
+number[1] = 77;
+number[3] = 44;
+console.log(number);
+
+//Find index of an element
+var positionIndex = number.indexOf(89);
+console.log(positionIndex);
