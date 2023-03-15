a0e165f1870594d9137c90ac69196b0c92171471
nakibcse <nakkibcse@gmail.com>
2023-03-15 11:49:22 +0600

    practice comparison statements in javascript

---
+++ JS-Array-Conditionals/comparison.js
@@ -0,0 +1,38 @@
+//Checks grater than
+console.log(5>6);
+
+//Checks less than
+console.log(5<6);
+
+//Checks equality
+console.log(5==6);
+console.log(6==6);
+
+//Checks not equal
+console.log(5!=6);
+
+//Checks less than or equal
+console.log(5<=6);
+console.log(6<=6);
+
+//Checks greater than or equal
+console.log(5>=2);
+console.log(2>=2);
+console.log(2>=2);
+
+
+var number1 = 56;
+var number2 = 89;
+
+console.log(number1>number2);
+console.log(number1<number2);
+console.log(number1===number2);
+console.log(number1!==number2);
+console.log(number1>=number2);
+console.log(number1<=number2);
+console.log(number1<=number2);
+
+
+//Multiple condition
+money1>money2 && result1>result2 && height1>height2 
+money1 > money2 || result1 > result2 || height1 > height2
