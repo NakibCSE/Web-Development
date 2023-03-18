8ac089ba85c84afe95bcc0d59c666c4aba05e457
Md. Nazim Uddin <nakkibcse@gmail.com>
2023-03-18 08:47:03 +0600

    for loop

--- JavaScript/loop-practice-problem/for.js
+++ JavaScript/loop-practice-problem/for.js
@@ -1,4 +1,15 @@
 var roastGiven = 0;
 while(roastGiven <7 ){
-    console.log('Roast Den, ')
+    console.log('Roast Den, Please!!!!!');
+    roastGiven++;
+}
+
+for(var roastGiven = 0; roastGiven<7; roastGiven++){
+    console.log('Roast Den, Please!!!!!');
+}
+
+
+//Simpler version of for loop
+for(i = 0; i<7 ; i++){
+    console.log(i);
 }
