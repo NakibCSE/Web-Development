c34fe29f1317ce6a2ee4573e53a2b68e716ede8a
nakibcse <nakkibcse@gmail.com>
2023-03-18 09:34:08 +0600

    traverse an array using for loop

---
+++ JavaScript/loop-practice-problem/array-loop.js
@@ -0,0 +1,6 @@
+// Target: printing all elements of an array
+
+var items = ['Bottle', 'Mouse', 'Sunglass','Pen','NoteBook'];
+for(var item = 0; item<items.length; item++){
+    console.log(items[item]);
+}
