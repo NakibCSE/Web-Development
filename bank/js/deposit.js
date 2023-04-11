6c8fd0a5dfa27afde8792f325907e6cc1d5f85cd
Md. Nazim Uddin <nakkibcse@gmail.com>
2023-04-11 07:57:25 +0600

    deposit and balance updated

--- bank/js/deposit.js
+++ bank/js/deposit.js
@@ -2,16 +2,30 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
     //Step 2: get the deposit amount
     const depositField = document.getElementById('depositField');
-    const depositAmount = depositField.value;
+    const newDepositAmountString= depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
     
     //Step 3: get the current deposit total
     const depositTotalElement = document.getElementById('deposit-total');
-    const PreviousDepositTotal = depositTotalElement.innerText;
+    const PreviousDepositTotalString = depositTotalElement.innerText;
+    const PreviousDepositTotal = parseFloat(PreviousDepositTotalString);
+
+
+    //Step 4: add numbers to set the total deposit
     const CurrentDepositTotal = PreviousDepositTotal + newDepositAmount;
-
-    depositTotalElement.innerText = depositAmount;
+    //Set the deposit total
+    depositTotalElement.innerText = CurrentDepositTotal;
     
+    //Step 5: get balance total 
+    const balanceTotalElement = document.getElementById('blance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal  = parseFloat(previousBalanceTotalString);
 
+
+    //Step 6: calculate current total
+    const currentBalanceTotal = previousBalanceTotal  + newDepositAmount;
+    //Set the balance total 
+    balanceTotalElement.innerText = currentBalanceTotal;
 
     //Step 7: Clear the deposit field
     depositField.value = '';
