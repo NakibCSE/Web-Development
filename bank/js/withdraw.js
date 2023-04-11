923245dd72f1c503146df20aafbe9cd97380aef5
nakibcse <nakkibcse@gmail.com>
2023-04-11 14:33:44 +0600

    withdraw feature added

--- bank/js/withdraw.js
+++ bank/js/withdraw.js
@@ -1,33 +1,41 @@
 //Step 1: Add event listener to withdraw button
-document.getElementById("btn-withdraw").addEventListener('click', function(){
+document.getElementById("btn-withdraw").addEventListener('click', function () {
 
     //Step 2: Get the withdraw amount
     const withdrawField = document.getElementById("withdraw-amnt");
     const withdrawAmntString = withdrawField.value;
     const newWithdrawAmnt = parseFloat(withdrawAmntString);
+    //step 8: clear the withdraw field
+    withdrawField.value = "";
+    if(isNaN(newWithdrawAmnt)){
+        alert("Please provide a valid amount");
+        return;
+    }
 
     //Step 3: Get the previous withdraw amnt
     const previousWithdrawField = document.getElementById("withdraw-total");
     const previousWithdrawAmntString = previousWithdrawField.innerText;
     const previousWithdrawAmnt = parseFloat(previousWithdrawAmntString);
-    
-    //Step 4: add the previous and new withdraw amnt to find total
-    const CurrentWithdrawAmnt = previousWithdrawAmnt + newWithdrawAmnt;
 
-    //Step 5: set the current withdraw amnt
-    previousWithdrawField.innerText = CurrentWithdrawAmnt;
+
 
     //Step 6: get the previous balance amount
     const PreviousBalanceField = document.getElementById("blance-total");
     const PreviousBalanceAmntString = PreviousBalanceField.innerText;
     const PreviousBalanceAmnt = parseFloat(PreviousBalanceAmntString);
 
+    if (newWithdrawAmnt > PreviousBalanceAmnt) {
+        alert("You can't with this amount!!!!!!!!!");
+        withdrawField.value = "";
+        return;
+    }
+    //Step 4: add the previous and new withdraw amnt to find total
+    const CurrentWithdrawAmnt = previousWithdrawAmnt + newWithdrawAmnt;
+
+    //Step 5: set the current withdraw amnt
+    previousWithdrawField.innerText = CurrentWithdrawAmnt;
     //Step 7: reduce the balance after withdrawal
     const currentBalance = PreviousBalanceAmnt - newWithdrawAmnt;
     PreviousBalanceField.innerText = currentBalance;
-
-
-    //step 8: clear the withdraw field
-    withdrawField.value = "";
 
 })
