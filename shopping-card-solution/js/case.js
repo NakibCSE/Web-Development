933056327c317fbae8edae02b18a96a7b34a63ac
nakibcse <nakkibcse@gmail.com>
2023-04-16 09:38:56 +0600

    case total price update added

--- shopping-card-solution/js/case.js
+++ shopping-card-solution/js/case.js
@@ -1,28 +1,48 @@
-// Add event listener on plus button of case field
-document.getElementById('case-plus').addEventListener('click', function(){
-    
-    //get the case number field's value
-    const caseNumberField = document.getElementById('case-number');
-    const previousCaseNumberString = caseNumberField.value;
-    const previousCaseNumber = parseInt(previousCaseNumberString);
-
-    //calculate current case number
-    const currentCaseNumber = previousCaseNumber + 1;
-
-    //set the current case number
-    caseNumberField.value = currentCaseNumber;
-})
-
-//Add event listener on minus button of case field 
-document.getElementById('case-minus').addEventListener('click', function(){
+//Common function for update case number
+function updateCaseNumber(isIncrease){
      //get the case number field's value
      const caseNumberField = document.getElementById('case-number');
      const previousCaseNumberString = caseNumberField.value;
      const previousCaseNumber = parseInt(previousCaseNumberString);
- 
+    
+     //Declare a updateable variable for new case number
+     let newCaseNumber;
+
      //calculate current case number
-     const currentCaseNumber = previousCaseNumber - 1;
- 
+     if(isIncrease){
+        newCaseNumber = previousCaseNumber + 1;
+     }
+     else{
+        newCaseNumber = previousCaseNumber - 1;
+     }
      //set the current case number
-     caseNumberField.value = currentCaseNumber;
+     caseNumberField.value = newCaseNumber;
+     return newCaseNumber;
+}
+
+// Add event listener on plus button of case field
+document.getElementById('case-plus').addEventListener('click', function(){
+    //Call case update function 
+    const totalCaseNumber = updateCaseNumber(true);
+
+    // Calculate new total price
+    const caseTotalPrice = totalCaseNumber * 59;
+
+    //Get the price total field and set the new total price
+    const caseTotalField = document.getElementById('case-total');
+    caseTotalField.innerText = caseTotalPrice;
+
+})
+
+//Add event listener on minus button of case field 
+document.getElementById('case-minus').addEventListener('click', function(){
+    //Call case update function 
+    const totalCaseNumber = updateCaseNumber(false);
+
+    // Calculate new total price
+    const caseTotalPrice = totalCaseNumber * 59;
+
+    //Get the price total field and set the new total price
+    const caseTotalField = document.getElementById('case-total');
+    caseTotalField.innerText = caseTotalPrice;
 })
