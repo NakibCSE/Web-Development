fef6be1ba6530162b70773339013be789fa7b802
nakibcse <nakkibcse@gmail.com>
2023-04-16 08:27:28 +0600

    case plus button added

---
+++ shopping-card-solution/js/case.js
@@ -0,0 +1,14 @@
+// Add event listener on plus button of case field
+document.getElementById('case-plus').addEventListener('click', function(){
+    
+    //get the case number field's value
+    const caseNumberField = document.getElementById('case-number');
+    const previousCaseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(previousCaseNumberString);
+
+    //calculate current case number
+    const currentCaseNumber = previousCaseNumber + 1;
+
+    //set the current case number
+    caseNumberField.value = currentCaseNumber;
+})
