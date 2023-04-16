5d345bf11916d147fd0cc3f1294ede06f8fb508b
nakibcse <nakkibcse@gmail.com>
2023-04-16 09:05:44 +0600

    case minus button added

--- shopping-card-solution/js/case.js
+++ shopping-card-solution/js/case.js
@@ -12,3 +12,17 @@ document.getElementById('case-plus').addEventListener('click', function(){
     //set the current case number
     caseNumberField.value = currentCaseNumber;
 })
+
+//Add event listener on minus button of case field 
+document.getElementById('case-minus').addEventListener('click', function(){
+     //get the case number field's value
+     const caseNumberField = document.getElementById('case-number');
+     const previousCaseNumberString = caseNumberField.value;
+     const previousCaseNumber = parseInt(previousCaseNumberString);
+ 
+     //calculate current case number
+     const currentCaseNumber = previousCaseNumber - 1;
+ 
+     //set the current case number
+     caseNumberField.value = currentCaseNumber;
+})
