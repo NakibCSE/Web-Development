5e77103eeef5457c379984082ea5d8178ac6c53a
unknown <m_nazimuddin@youngonectg.com>
2023-05-03 09:08:59 +0600

    DOM practice

--- es6-intro/js/dom.js
+++ es6-intro/js/dom.js
@@ -9,3 +9,14 @@ document.getElementById('third-center').addEventListener('click', function(){
     const third = document.getElementById('third-friend');
     third.style.textAlign = 'center';
 })
+
+document.getElementById('new-friend').addEventListener('click',function(){
+    const friendContainer = document.getElementById('friends');
+    const friend = document.createElement('div');
+    friend.classList.add('friend');
+    friend.innerHTML = `
+        <h3 class="friend-name">New Friend</h3>
+        <p>Something new added</p>
+    `
+    friendContainer.appendChild(friend);
+})
